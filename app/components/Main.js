f36317021ef88dcc9c900ae6ef4e26d863d45e4c
Kévin Gossé <gossekevin1@gmail.com>
2016-02-25 23:12:04 +0100

    add basic component

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Main extends React.Component {
+    render(){
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
+
+
+export default Main;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    context: path.join(__dirname, 'app'),
+    entry: './index.js',
+    output: {
+        filename: 'public/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+
+        ]
+    }
+};
