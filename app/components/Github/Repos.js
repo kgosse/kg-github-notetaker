877bd0fb25f1043889956f4e0163b66dad8f2b58
Kévin Gossé <gossekevin1@gmail.com>
2016-03-18 07:27:23 +0100

    Render userprofile and repos with dynamic data

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -6,10 +6,21 @@ import React, { Component } from 'react';
 
 class Repos extends Component {
     render(){
-        console.log('REPOS', this.props.repos);
+        let repos = this.props.repos.map(function(repo, index){
+            return (
+                <li className="list-group-item" key={index}>
+                    {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+                    {repo.description && <p>{repo.description}</p>}
+                </li>
+            )
+        });
+
         return (
             <div>
-                <p>REPOS</p>
+                <h3> User Repos </h3>
+                <ul className="list-group">
+                    {repos}
+                </ul>
             </div>
         )
     }
